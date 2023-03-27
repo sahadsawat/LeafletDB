ab140383942073675f28d0683cb785bd66a6877e
sahadsawat <mr.fapman_nongnine@hotmail.com>
2023-03-27 22:59:36 +0700

    add getseeobj

--- getSeeobjWhereSeeobj.php
+++ getSeeobjWhereSeeobj.php
@@ -19,14 +19,14 @@ if (!$link->set_charset("utf8")) {
 if (isset($_GET)) {
 	if ($_GET['isAdd'] == 'true') {
 				
-		$reportobj_status = $_GET['reportobj_status'];
+		$seeobj_status = $_GET['seeobj_status'];
 
 		$result = mysqli_query($link, "SELECT t1.*,t2.locat_name,t3.cate_name,t4.user_email
-		FROM db_app_leaflet.reportobj t1
+		FROM db_app_leaflet.seeobj t1
 		LEFT JOIN db_app_leaflet.location t2 ON t2.locat_id = t1.locat_id
 		LEFT JOIN db_app_leaflet.category t3 ON t3.cate_id = t1.cate_id
 		LEFT JOIN db_app_leaflet.user t4 ON t4.user_id = t1.user_id
-		WHERE t1.reportobj_status = '$reportobj_status'");
+		WHERE t1.seeobj_status = '$seeobj_status'");
 
 		if ($result) {
 
